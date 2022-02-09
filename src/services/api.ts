20dceba8b609ce6933cf6e68404566f1945db018
Arthur Lopes <arthurllopes10@gmail.com>
2022-02-09 12:10:23 -0300

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_PRODUCTION_DOMAIN}/api`
+    baseURL: `${process.env.NEXT_PUBLIC_DOMAIN}/api`
 })
 
 export default api;
